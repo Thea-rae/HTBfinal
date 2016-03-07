a11a18ab323195cfcaedd0730cae07c2a2d89b41
thea <tr@thearae.com>
2016-03-07 12:52:44 -0500

    message and hidden working in CS

--- content.js
+++ content.js
@@ -1,42 +1,43 @@
-console.log("fuck you chrome");
+chrome.runtime.onMessage.addListener(
+	function(message, sender, sendResponse) {
+		console.log('Got message from background: ', message);
+		if (message){
+			console.log("fuck social media");
+		}
+	}
+);
 
-// chrome.runtime.onMessage.addListener(
-// 	function(message, sender, sendResponse) {
-// 		console.log('Got message from background: ', message);
-// 	}
-// };
+function getHiddenProp(){
+		var prefixes = ['webkit','moz','ms','o'];
+		if ('hidden' in document) return 'hidden';
+		for (var i = 0; i < prefixes.length; i++){
+				if ((prefixes[i] + 'Hidden') in document) 
+						return prefixes[i] + 'Hidden';
+		}
+		return null;
+}
 
-// function getHiddenProp(){
-// 		var prefixes = ['webkit','moz','ms','o'];
-// 		if ('hidden' in document) return 'hidden';
-// 		for (var i = 0; i < prefixes.length; i++){
-// 				if ((prefixes[i] + 'Hidden') in document) 
-// 						return prefixes[i] + 'Hidden';
-// 		}
-// 		return null;
-// }
+function isHidden() {
+	var prop = getHiddenProp();
+	if (!prop) return false;
+	return document[prop];
+}
 
-// function isHidden() {
-// 	var prop = getHiddenProp();
-// 	if (!prop) return false;
-// 	return document[prop];
-// }
+var visProp = getHiddenProp();
+if (visProp) {
+	var evtname = visProp.replace(/[H|h]idden/,'') + 'visibilitychange';
+	document.addEventListener(evtname, visChange);
+	console.log("bitches");
+}
 
-// var visProp = getHiddenProp();
-// if (visProp) {
-// 	var evtname = visProp.replace(/[H|h]idden/,'') + 'visibilitychange';
-// 	document.addEventListener(evtname, visChange);
-// 	console.log("bitches");
-// }
-
-// function visChange(){
-// 	console.log("i got called");
-// 	var hide;
-// 	if (isHidden()){
-// 		console.log(hide);
-// 		hide = true;
-// 	}else{
-// 		hide = false;
-// 		console.log(hide);
-// 	}
-// }
+function visChange(){
+	console.log("i got called");
+	var hide;
+	if (isHidden()){
+		hide = true;
+		console.log(hide);
+	}else{
+		hide = false;
+		console.log(hide);
+	}
+}
